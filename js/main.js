a8e7ee671d1a4fb217b2c73ccc7ee6801f2757b9
Yemelin <toolive@list.ru>
2015-06-19 12:25:26 +0300

    learn the data types

--- js/main.js
+++ js/main.js
@@ -1,4 +1,41 @@
+"use strict";
+
 /**
  * Created by d.emelin on 18.06.15.
  */
 console.log("Script is working!");
+/* alert("ща ошибка будет");
+ [1, 2].forEach(alert)*/
+// пробуем вставлять комменты
+/* кажется комменты тут как и в HTML
+ это многострочноый комментарий */
+
+/* ниже уже более новый код
+ var message; // объявили переменную
+ message = 'Hello'; //сохраняем переменную в строку.
+ alert(message); // выведет содержание переменной */
+
+var userName = 'John', age = 25, message = 'hello'; // несколько переменных сразу
+
+var x;
+// alert( x ); // выведет "undefined"
+var user = {name: "Вася"};
+
+
+typeof undefined
+var a =1;
+var b = 2;
+var c = 4 - (a=b+2);
+console.log(c);
+console.log(10%3);
+console.log(13%5);
+console.log(97%13);
+console.log("");
+var i = 3;
+console.log(--i);
+i = 3;
+console.log(i--);
+
+a = (5, 6);
+
+alert( a );
