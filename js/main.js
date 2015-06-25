5d3285c46bbd9a20448c7807d31df09e768c5e01
Yemelin <toolive@list.ru>
2015-06-25 12:39:57 +0300

    Loops - while, for

--- js/main.js
+++ js/main.js
@@ -5,57 +5,50 @@
  */
 console.log("Script is working!");
 
-///////// Logical operators
+///////// Loops - while, for
+/*
+ var sum = 0;
 
-/*var hour = 9;
-if (hor <10 || > 18) {
-    console.log("Office is working between 10 and 18 hours")
+ while (true) {
+
+ var value = +prompt("Введите число", '');
+
+ if (!value) break; // (*)
+
+ sum += value;
+ console.log(sum);
+ }
+ alert( 'Сумма: ' + sum );
+ */
+
+/*
+for (var i = 0; i < 10; i++) {
+    if (i%2 ==0) continue;
+    console.log(i);
 }*/
 
-/*alert( alert(1) || 2 || alert(3) );*/
-/*var age = prompt('age');
-if (age >=14 && age <=90) {
-    console.log("good");
+/*
+for (var i = 2; i<=10; i++) {
+    if (i%2>0) continue;
+console.log(i);
 }
-else {
-    console.log("bad");
-}*/
-//Напишите условие if для проверки того факта, что age НЕ находится между 14 и 90 включительно.
-/*var age = prompt('age');
-
-if (!(age >=14 && age <=90)) {
-    console.log("good");
-}
-else {
-    console.log("bad");
+*/
+/*for (var i = 0; i < 3; i++) {
+    console.log( "номер " + i + "!" );
 }*/
 /*
-if (age <14 || age >90) {
-    console.log("good");
-}
-else {
-    console.log("bad");
-}*/
+var i=0;
+while (i++<3) console.log( "номер " + i + "!" );*/
+/*
 
+var i;
+do {i = prompt("Enter number more than 100",0);
+} while (i<=100 && i != null);*/
 
-///Type conversion for primitives
-console.log("" + 1 + 0); // "10"
-console.log("" - 1 + 0); // -1
-console.log(true + false); // 1
-console.log(6 / "3"); // 2
-console.log("2" * "3");// 6
-console.log(4 + 5 + "px");// "9px"
-console.log("$" + 4 + 5);// "$45"
-console.log("4" - 2);// 2
-console.log("4px" -2);// nan
-console.log(7/0); //infinity
-console.log("  -9\n" + 5);// "  -9\n5"
-console.log("  -9\n" - 5);// -14
-console.log(5 && 2);// 2
-console.log(2 && 5);// 5
-console.log(5 || 0);// 5
-console.log(0 || 5);// 5
-console.log(null + 1);//1
-console.log(undefined + 1);// nan
-console.log(null == "\n0\n");// false
-console.log(+null == +"\n0\n"); // true
+nextPrime:
+    for (var i = 1; i < 10; i++) {
+        for (var j = 2; j < i; j++) {
+            if (i % j == 0) continue nextPrime;
+        }
+        console.log( i ); // простое
+    }
